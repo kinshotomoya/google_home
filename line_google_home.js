5cd1c72d3e68862ca0154d19caac26746f54ad16
金正朋也 <mhtnim1023@gmail.com>
2019-01-12 14:54:01 +0900

    moduleのinstall

--- line_google_home.js
+++ line_google_home.js
@@ -1,5 +1,4 @@
 const http = require('http');
-require('dotenv').config();
 const google_home = require('google-home-notifier');
 const language = 'ja';
 
@@ -8,9 +7,23 @@ var server = http.createServer();
 // 3000ポートで待ち受けして、受け入れ先
 server.on('request', (req, res) => {
     req.on('data', (message) => {
-        console.log(JSON.parse(message).events[0].message.text);
+        let userId = JSON.parse(message).events[0].source.userId;
+        if (userId == "Uaacd580dab2c95d8d7b907c76ea625b7") {
+            var userName = 'トモヤからのメッセージです。';
+        } else if (userId == 'U362a6c43831faaf200e8ef4daea3f3e6') {
+            userName = "ママからのメッセージです。";
+        } else if (userId == 'Uf8133ed6ed6dc6b34d8646ba33de303f') {
+            userName = 'ななからのメッセージです。'
+        } else if (userId == 'Ub8e48bbd0b4dea8ea0f6984a8ea0bfa2') {
+            userName = 'いくやからのメッセージです。'
+        } else if (userId == 'U99a4a2253412238a0b510cacd34d39ff') {
+            userName = 'みみからのメッセージです。'
+        };
+        console.log(userName);
+        console.log(JSON.parse(message).events[0].source.userId);
         let send_message = JSON.parse(message).events[0].message.text;
-        google_home.ip('sssss', language);
+        send_message = userName + send_message;
+        google_home.device('Google-Home', language);
         google_home.notify(send_message, (res) => {
             console.log(res);
         });
